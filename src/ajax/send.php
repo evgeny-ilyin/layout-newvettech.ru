a80dcbf9b88c37e4c04127558302fbcd3df1a13f
Evgeny Ilyin <evgeniy.v.ilyin@gmail.com>
2025-11-17 22:26:15 +0300

    скрипт для тестового вывода результатов отправки формы

---
+++ src/ajax/send.php
@@ -0,0 +1,7 @@
+<?
+header("Access-Control-Allow-Origin: *");
+
+// die();
+
+// echo json_encode(["success" => false, "message" => "Непредвиденная ошибка"]);
+echo json_encode(["success" => true, "message" => "Спасибо, ваш запрос принят, в&nbsp;ближайшее время мы&nbsp;вам перезвоним"]);
