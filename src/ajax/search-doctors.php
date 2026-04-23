94d350759a9d6508d8ea72a1f72c1a7bc8c33619
Evgeny Ilyin <evgeniy.v.ilyin@gmail.com>
2026-04-23 13:15:33 +0300

    тестовый скрипт выдачи результатов для поиска по submit

---
+++ src/ajax/search-doctors.php
@@ -0,0 +1,92 @@
+<?
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
+$result = '				<div class="doctors-list js-results" data-more-url="/local/ajax/load-more-doctors.php">
+					<div class="doctor-card" id="bx_651765591_2031">
+						<a href="/vrachi/razumova-anna-dmitrievna/" class="doctor-card__link" aria-label="Подробнее о враче Разумова Анна Дмитриевна">
+							<div class="doctor-card__photo">
+								<img src="./img/dev/placeholder-man.png" width="400" height="400" alt="" loading="lazy" decoding="async">
+							</div>
+							<div class="doctor-card__text">
+								<div class="doctor-card__name h5">Разумова Анна Дмитриевна</div>
+								<div class="doctor-card__position">Ветеринарный врач-гастроэнтеролог</div>
+								<div class="doctor-card__location">
+									<div class="location">м. Серпуховская</div>
+								</div>
+							</div>
+						</a>
+						<div class="doctor-card__button">
+							<button type="button" class="btn btn_md btn_white btn_wide js-metapets" aria-label="Записаться к врачу Разумова Анна Дмитриевна">Записаться</button>
+						</div>
+					</div>
+					<div class="doctor-card" id="bx_651765591_2127">
+						<a href="/vrachi/yurkova-anna-sergeevna/" class="doctor-card__link" aria-label="Подробнее о враче Юркова Анна Сергеевна">
+							<div class="doctor-card__photo">
+								<img src="./img/dev/placeholder-man.png" width="400" height="400" alt="" loading="lazy" decoding="async">
+							</div>
+							<div class="doctor-card__text">
+								<div class="doctor-card__name h5">Юркова Анна Сергеевна</div>
+								<div class="doctor-card__position">Ветеринарный врач–хирург</div>
+								<div class="doctor-card__location">
+									<div class="location">м. Серпуховская</div>
+								</div>
+							</div>
+						</a>
+						<div class="doctor-card__button">
+							<button type="button" class="btn btn_md btn_white btn_wide js-metapets" aria-label="Записаться к врачу Юркова Анна Сергеевна">Записаться</button>
+						</div>
+					</div>
+					<div class="doctor-card" id="bx_651765591_2143">
+						<a href="/vrachi/bunakova-elena-romanovna/" class="doctor-card__link" aria-label="Подробнее о враче Бунакова Елена Романовна">
+							<div class="doctor-card__photo">
+								<img src="./img/dev/placeholder-man.png" width="400" height="400" alt="" loading="lazy" decoding="async">
+							</div>
+							<div class="doctor-card__text">
+								<div class="doctor-card__name h5">Бунакова Елена Романовна</div>
+								<div class="doctor-card__position">Ветеринарный врач - хирург</div>
+								<div class="doctor-card__location">
+									<div class="location">м. Серпуховская</div>
+								</div>
+							</div>
+						</a>
+						<div class="doctor-card__button">
+							<button type="button" class="btn btn_md btn_white btn_wide js-metapets" aria-label="Записаться к врачу Бунакова Елена Романовна">Записаться</button>
+						</div>
+					</div>
+					<div class="doctor-card" id="bx_651765591_2157">
+						<a href="/vrachi/boyko-anna-vladimirovna/" class="doctor-card__link" aria-label="Подробнее о враче Бойко Анна Владимировна">
+							<div class="doctor-card__photo">
+								<img src="./img/dev/placeholder-man.png" width="400" height="400" alt="" loading="lazy" decoding="async">
+							</div>
+							<div class="doctor-card__text">
+								<div class="doctor-card__name h5">Бойко Анна Владимировна</div>
+								<div class="doctor-card__position">Ветеринарный врач-анестезиолог</div>
+								<div class="doctor-card__location">
+									<div class="location">м. Серпуховская</div>
+								</div>
+							</div>
+						</a>
+						<div class="doctor-card__button">
+							<button type="button" class="btn btn_md btn_white btn_wide js-metapets" aria-label="Записаться к врачу Бойко Анна Владимировна">Записаться</button>
+						</div>
+					</div>
+					<div class="doctor-card" id="bx_651765591_2272">
+						<a href="/vrachi/savonkina-anna-romanovna-/" class="doctor-card__link" aria-label="Подробнее о враче Савонкина Анна Романовна ">
+							<div class="doctor-card__photo">
+								<img src="./img/dev/placeholder-man.png" width="400" height="400" alt="" loading="lazy" decoding="async">
+							</div>
+							<div class="doctor-card__text">
+								<div class="doctor-card__name h5">Савонкина Анна Романовна </div>
+								<div class="doctor-card__position">Ассистент ветеринарного врача </div>
+								<div class="doctor-card__location">
+									<div class="location">м. Серпуховская</div>
+								</div>
+							</div>
+						</a>
+					</div>
+				</div>
+';
+
+
+echo $result;
